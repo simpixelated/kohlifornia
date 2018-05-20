362033b3f7259c944891da2ab346b26c660bd785
Jordan Kohl <kohl.jordan@gmail.com>
2018-05-20 13:50:55 -0700

    replaces react-ga with gatsby ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `119389354-1`,
       },
     },
     `gatsby-plugin-feed`,
